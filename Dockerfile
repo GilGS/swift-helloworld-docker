dd28ecf4d96b15e0aecb94bafd7b695a3db1fdb1
rolivieri <roliv@us.ibm.com>
2016-03-01 13:46:29 -0600

    Updates to dockerfile... now adding the GitHub repo at docker image creation time.

--- Dockerfile
+++ Dockerfile
@@ -23,6 +23,12 @@ LABEL Description="Image to run the Swift Sample Starter App inside an IBM Conta
 
 EXPOSE 9080
 
+# Clone Swift Starter App repo
+git clone -b master https://github.com/IBM-Bluemix/swift-helloworld
+
+# Build Swift Started App
+cd /root/swift-helloworld && swift build
+
 # Add build files to image
 ADD start-swift-helloworld.sh /root
 
--- start-swift-helloworld.sh
+++ start-swift-helloworld.sh
@@ -23,14 +23,5 @@
 # If any commands fail, we want the shell script to exit immediately.
 set -e
 
-echo ">> About to clone Swift Sample Starter App..."
-# Clone Swift Starter App repo
-cd /root && rm -rf swift-helloworld && git clone -b master https://github.com/IBM-Bluemix/swift-helloworld
-
-# Build Swift Starter App
-echo ">> About to build Swift Sample Starter App..."
-cd /root/swift-helloworld && swift build
-echo ">> Build for Swift Sample Starter App completed."
-
 # Run sample server
 cd /root/swift-helloworld && .build/debug/Server
